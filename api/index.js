24737a3c81c3540fcdc8cbf72d8a2f53bd22111c
Kanishka <kanishkadilshan861@gmail.com>
2024-03-12 14:51:46 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect
+("mongodb+srv://kanishka:dilshan@mern-estate-c.vsxarxn.mongodb.net/mern-estate-c?retryWrites=true&w=majority&appName=mern-estate-c")
+.then (()=> {
+    console.log("Database connected");   {/*to check whether database is connected or not*/}
+}).catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 
